584459b974dbb5283fc9654424cb339f3254f9ea
shahbozshirinboyev <shahboz.sh.b@gmail.com>
2025-03-13 16:19:06 +0500

    work section version v1.0

--- src/App.jsx
+++ src/App.jsx
@@ -320,80 +320,185 @@ function App() {
       {/* /Services */}
 
       {/* Rusume */}
-        <div className="min-h-[500px] flex justify-start items-center">
-          <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 container mx-auto lg:px-18 xl:px-32 items-start">
-            <div>
-              <h2 className="text-4xl">Why hire me?</h2>
-              <p className="mt-4">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ab, ipsum!</p>
+      <div className="min-h-[500px] flex justify-start items-center">
+        <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 container mx-auto lg:px-18 xl:px-32 items-start">
+          <div>
+            <h2 className="text-4xl">Why hire me?</h2>
+            <p className="mt-4">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ab,
+              ipsum!
+            </p>
 
-              <div className="flex flex-col gap-2 mt-8">
-                <button className="btn w-full">Experience</button>
-                <button className="btn w-full">Education</button>
-                <button className="btn w-full">Skills</button>
-                <button className="btn w-full">About me</button>
-              </div>
+            <div className="flex flex-col gap-2 mt-8">
+              <button className="btn w-full">Experience</button>
+              <button className="btn w-full">Education</button>
+              <button className="btn w-full">Skills</button>
+              <button className="btn w-full">About me</button>
             </div>
-            <div className="lg:col-span-2">
-              <div className="px-4">
+          </div>
+          <div className="lg:col-span-2">
+            <div className="px-4">
               <h2 className="text-3xl">My experience</h2>
-              <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Similique hic earum doloremque molestiae, quisquam quia deleniti! Repellat nesciunt tenetur neque?</p>
-              </div>
+              <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                Similique hic earum doloremque molestiae, quisquam quia
+                deleniti! Repellat nesciunt tenetur neque?
+              </p>
+            </div>
 
-              <div className="grid grid-cols-2 h-[300px] overflow-scroll overflow-x-hidden p-4 gap-3">
-                <div className="w-full p-4 border border-gray-300 bg-gray-100 rounded-2xl">
-                  <span>2019-202</span>
-                  <h4>Teaching Assistant</h4>
-                  <ul className="mt-8">
-                    <li>Teach Academy</li>
-                  </ul>
-                </div>
-                <div className="w-full p-4 border border-gray-300 bg-gray-100 rounded-2xl">
-                  <span>2019-202</span>
-                  <h4>Teaching Assistant</h4>
-                  <ul className="mt-8">
-                    <li>Teach Academy</li>
-                  </ul>
-                </div>
-                <div className="w-full p-4 border border-gray-300 bg-gray-100 rounded-2xl">
-                  <span>2019-202</span>
-                  <h4>Teaching Assistant</h4>
-                  <ul className="mt-8">
-                    <li>Teach Academy</li>
-                  </ul>
-                </div>
-                <div className="w-full p-4 border border-gray-300 bg-gray-100 rounded-2xl">
-                  <span>2019-202</span>
-                  <h4>Teaching Assistant</h4>
-                  <ul className="mt-8">
-                    <li>Teach Academy</li>
-                  </ul>
-                </div>
-                <div className="w-full p-4 border border-gray-300 bg-gray-100 rounded-2xl">
-                  <span>2019-202</span>
-                  <h4>Teaching Assistant</h4>
-                  <ul className="mt-8">
-                    <li>Teach Academy</li>
-                  </ul>
-                </div>
-                <div className="w-full p-4 border border-gray-300 bg-gray-100 rounded-2xl">
-                  <span>2019-202</span>
-                  <h4>Teaching Assistant</h4>
-                  <ul className="mt-8">
-                    <li>Teach Academy</li>
-                  </ul>
-                </div>
-                <div className="w-full p-4 border border-gray-300 bg-gray-100 rounded-2xl">
-                  <span>2019-202</span>
-                  <h4>Teaching Assistant</h4>
-                  <ul className="mt-8">
-                    <li>Teach Academy</li>
-                  </ul>
-                </div>
+            <div className="grid grid-cols-2 h-[300px] overflow-scroll overflow-x-hidden p-4 gap-3">
+              <div className="w-full p-4 border border-gray-300 bg-gray-100 rounded-2xl">
+                <span>2019-202</span>
+                <h4>Teaching Assistant</h4>
+                <ul className="mt-8">
+                  <li>Teach Academy</li>
+                </ul>
+              </div>
+              <div className="w-full p-4 border border-gray-300 bg-gray-100 rounded-2xl">
+                <span>2019-202</span>
+                <h4>Teaching Assistant</h4>
+                <ul className="mt-8">
+                  <li>Teach Academy</li>
+                </ul>
+              </div>
+              <div className="w-full p-4 border border-gray-300 bg-gray-100 rounded-2xl">
+                <span>2019-202</span>
+                <h4>Teaching Assistant</h4>
+                <ul className="mt-8">
+                  <li>Teach Academy</li>
+                </ul>
+              </div>
+              <div className="w-full p-4 border border-gray-300 bg-gray-100 rounded-2xl">
+                <span>2019-202</span>
+                <h4>Teaching Assistant</h4>
+                <ul className="mt-8">
+                  <li>Teach Academy</li>
+                </ul>
+              </div>
+              <div className="w-full p-4 border border-gray-300 bg-gray-100 rounded-2xl">
+                <span>2019-202</span>
+                <h4>Teaching Assistant</h4>
+                <ul className="mt-8">
+                  <li>Teach Academy</li>
+                </ul>
+              </div>
+              <div className="w-full p-4 border border-gray-300 bg-gray-100 rounded-2xl">
+                <span>2019-202</span>
+                <h4>Teaching Assistant</h4>
+                <ul className="mt-8">
+                  <li>Teach Academy</li>
+                </ul>
+              </div>
+              <div className="w-full p-4 border border-gray-300 bg-gray-100 rounded-2xl">
+                <span>2019-202</span>
+                <h4>Teaching Assistant</h4>
+                <ul className="mt-8">
+                  <li>Teach Academy</li>
+                </ul>
               </div>
             </div>
           </div>
         </div>
+      </div>
       {/* /Rusume */}
+
+      {/* Work */}
+      <div className="min-h-[500px] flex justify-start items-center">
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 container mx-auto lg:px-18 xl:px-32 items-start">
+          <div className="w-full">
+            <span className="text-6xl">01</span>
+            <h2 className="text-2xl py-2">Frontend Project</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit
+              magni porro, nobis dignissimos reprehenderit totam.
+            </p>
+            <div className="flex gap-3 py-2">
+              <span className="border rounded-2xl px-2 py-1 text-[12px]">
+                Html 5
+              </span>
+              <span className="border rounded-2xl px-2 py-1 text-[12px]">
+                CSS 3
+              </span>
+              <span className="border rounded-2xl px-2 py-1 text-[12px]">
+                Javascript
+              </span>
+            </div>
+            <hr />
+            <div className="flex gap-2 py-2">
+              <button className="btn btn-circle">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  class="bi bi-arrow-right -rotate-45"
+                  viewBox="0 0 16 16"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8"
+                  />
+                </svg>
+              </button>
+              <button className="btn btn-circle">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  class="bi bi-github"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27s1.36.09 2 .27c1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.01 8.01 0 0 0 16 8c0-4.42-3.58-8-8-8" />
+                </svg>
+              </button>
+            </div>
+          </div>
+
+          <div className="w-full">
+            <div className="w-full">
+              <img
+                className="w-full h-[300px] rounded-2xl"
+                src="https://picsum.photos/500/600/?blur"
+                alt=""
+              />
+            </div>
+            <div className="flex gap-3 mt-4">
+              <button className="btn btn-circle">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  class="bi bi-arrow-left"
+                  viewBox="0 0 16 16"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8"
+                  />
+                </svg>
+              </button>
+              <button className="btn btn-circle">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  class="bi bi-arrow-right"
+                  viewBox="0 0 16 16"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8"
+                  />
+                </svg>
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* /Work */}
     </>
   );
 }
