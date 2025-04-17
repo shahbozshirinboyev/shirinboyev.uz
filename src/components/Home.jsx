cbd6637ce2b8c9c33c62cb5952e5ca62f3f364c5
shahbozshirinboyev <shahboz.sh.b@gmail.com>
2025-04-17 14:12:37 +0500

    Home section ...

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,16 +4,17 @@ import Statistic from './Statistic/Statistic'
 function Home() {
   return (
     <>
-      <div className='container mx-auto px-2 tablet:px-0 flex flex-col laptop:flex-row gap-2 border my-12'>
-        <div className='border w-full'>
+      <div className='container mx-auto px-2 tablet:px-0 flex flex-col laptop:flex-row gap-4 my-12'>
 
-          <div>
-            <span>Software Engineer</span>
-            <h2>Assalomu alaykum! Men Shahboz Shirinboyev!</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos rerum autem eaque quod et eligendi architecto maxime fugit.</p>
+        <div className='w-full border'>
+
+          <div className='text-center laptop:text-start'>
+            <span className='font-semibold'>Software Engineer</span>
+            <h2 className='font-extrabold text-5xl'><span className='text-4xl'>Assalomu alaykum!</span><br />Men Shahboz Shirinboyev.</h2>
+            <p className='font-normal mt-2'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos rerum autem eaque quod et eligendi architecto maxime fugit.</p>
           </div>
 
-          <div className='flex gap-2'>
+          <div className='flex gap-2 mt-5 justify-center laptop:justify-start'>
             <button className='btn rounded-full'>Download CV</button>
             <button className='btn btn-circle'>G</button>
             <button className='btn btn-circle'>T</button>
