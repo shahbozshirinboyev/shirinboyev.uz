d8bdd762b4c91689fdd4aba705e3cb0b6ba9a1fe
shahbozshirinboyev <shahboz.sh.b@gmail.com>
2025-04-17 17:20:16 +0500

    New section is Start

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import Home from './components/Home'
+import Services from './components/Services/Services'
 
 function App() {
   return (
     <>
       <Navbar />
       <Home />
+      <Services />
       <div className='container mx-auto px-2 tablet:px-0 h-screen border'>App</div>
       <div className='container mx-auto px-2 tablet:px-0 h-screen border'>App</div>
     </>
---
+++ src/components/Services/Services.jsx
@@ -0,0 +1,58 @@
+import React from 'react'
+
+function Services() {
+  return (
+    <div className='container mx-auto px-2 tablet:px-0 grid grid-cols-1 laptop:grid-cols-2 gap-12 my-12'>
+      <div className=''>
+        <div className='flex justify-between items-center group'>
+          <span className='text-5xl font-extrabold'>01</span>
+          <div className='rotate-45 group-hover:rotate-0 transition-all duration-300'>
+            <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" class="bi bi-arrow-right-circle-fill" viewBox="0 0 16 16">
+              <path d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0M4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5z"/>
+            </svg>
+          </div>
+        </div>
+        <h2 className='text-5xl font-extrabold'>Web Development</h2>
+        <p className='mt-12 text-lg opacity-50'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est aut odio illum. Cumque, ad facere.</p>
+      </div>
+      <div className=''>
+        <div className='flex justify-between items-center group'>
+          <span className='text-5xl font-extrabold'>01</span>
+          <div className='rotate-45 group-hover:rotate-0 transition-all duration-300'>
+            <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" class="bi bi-arrow-right-circle-fill" viewBox="0 0 16 16">
+              <path d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0M4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5z"/>
+            </svg>
+          </div>
+        </div>
+        <h2 className='text-5xl font-extrabold'>Web Development</h2>
+        <p className='mt-12 text-lg opacity-50'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est aut odio illum. Cumque, ad facere.</p>
+      </div>
+      <div className=''>
+        <div className='flex justify-between items-center group'>
+          <span className='text-5xl font-extrabold'>01</span>
+          <div className='rotate-45 group-hover:rotate-0 transition-all duration-300'>
+            <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" class="bi bi-arrow-right-circle-fill" viewBox="0 0 16 16">
+              <path d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0M4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5z"/>
+            </svg>
+          </div>
+        </div>
+        <h2 className='text-5xl font-extrabold'>Web Development</h2>
+        <p className='mt-12 text-lg opacity-50'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est aut odio illum. Cumque, ad facere.</p>
+      </div>
+      <div className=''>
+        <div className='flex justify-between items-center group'>
+          <span className='text-5xl font-extrabold'>01</span>
+          <div className='rotate-45 group-hover:rotate-0 transition-all duration-300'>
+            <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" class="bi bi-arrow-right-circle-fill" viewBox="0 0 16 16">
+              <path d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0M4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5z"/>
+            </svg>
+          </div>
+        </div>
+        <h2 className='text-5xl font-extrabold'>Web Development</h2>
+        <p className='mt-12 text-lg opacity-50'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est aut odio illum. Cumque, ad facere.</p>
+      </div>
+    </div>
+  )
+}
+
+export default Services
