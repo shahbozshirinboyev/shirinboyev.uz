8ee9238966c278a24936bbb51dbb96ba638a14a9
shahbozshirinboyev <shahboz.sh.b@gmail.com>
2025-07-07 19:43:43 +0500

    new section ...

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,30 +14,30 @@ function Navbar() {
       transition={transition}
       className="container mx-auto px-2 tablet:px-0 my-5 sticky top-2">
 
-      <div className="flex justify-between items-center rounded-full px-3 py-1 bg-gradient-to-r from-[#fdfbfb] via-[#ebedee] to-[#dfe9f3] border border-gray-200 dark:border-gray-600 dark:bg-gradient-to-r dark:from-[#0f2027] dark:via-[#203a43] dark:to-[#2c5364]">
+      <div className="flex justify-between items-center rounded-full px-3 py-2 border border-zinc-200 bg-white/70 backdrop-blur-sm">
 
         <div className="">
-          <img src="/images/logo/logo.png" className="w-10" alt="logo" />
+          {/* <img src="/images/logo/logo.png" className="w-10" alt="logo" /> */}
+          <h4 className="font-bold">Shahboz<span className="text-green-600">.</span></h4>
         </div>
 
         <div className="flex items-center justify-center gap-2">
 
-          <ThemeSwitcher />
+          {/* <ThemeSwitcher /> */}
 
-          <ul className="hidden tablet:flex gap-4 dark:text-white">
-            <li>Home</li>
-            <li>Services</li>
-            <li>Resume</li>
-            <li>Work</li>
-            <li>Contact</li>
-            <li>Hire Me</li>
+          <ul className="hidden tablet:flex gap-4">
+            <li> <a href="" className="font-semibold hover:text-green-600 transition-all duration-300">Home</a></li>
+            <li> <a href="" className="font-semibold hover:text-green-600 transition-all duration-300">Services</a></li>
+            <li> <a href="" className="font-semibold hover:text-green-600 transition-all duration-300">Resume</a></li>
+            <li> <a href="" className="font-semibold hover:text-green-600 transition-all duration-300">Work</a></li>
+            <li> <a href="" className="font-semibold hover:text-green-600 transition-all duration-300">Contact</a></li>
           </ul>
 
-          <div className="dropdown dropdown-end">
-            <div tabIndex={0} role="button" className="btn btn-circle bg-transparent border-0 shadow-none">
+          <div className="dropdown dropdown-end tablet:hidden">
+            <div tabIndex={0} role="button" className="btn btn-circle btn-sm bg-transparent border-0 shadow-none">
               <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" className="bi bi-list" viewBox="0 0 16 16"> <path fillRule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5" /> </svg>
             </div>
-            <ul tabIndex={0} className="dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm">
+            <ul tabIndex={0} className="dropdown-content menu bg-white backdrop-blur-sm rounded-box z-1 w-52 p-2 shadow-sm">
               <li><a>Home</a></li>
               <li><a>Services</a></li>
               <li><a>Resume</a></li>
