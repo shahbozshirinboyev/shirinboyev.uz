4b12762758f2723a44bc70d02fa4de1c4f8eec7f
shahbozshirinboyev <shahboz.sh.b@gmail.com>
2025-07-10 12:50:58 +0500

    add new ...

--- src/components/Works/Works.jsx
+++ src/components/Works/Works.jsx
@@ -2,11 +2,10 @@ import { useState } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import { useTranslation } from "react-i18next";
 
-
 function Works() {
-    const { t } = useTranslation();
+  const { t } = useTranslation();
   const works = t("works", { returnObjects: true });
-const projects = works.items
+  const projects = works.items;
   const [currentIndex, setCurrentIndex] = useState(0);
 
   const handlePrev = () => {
@@ -20,10 +19,7 @@ const projects = works.items
   const project = projects[currentIndex];
 
   return (
-    <div
-      id="works"
-      className="c_container"
-    >
+    <div id="works" className="c_container">
       <div className="border border-green-200 dark:border-green-200/10 bg-green-50/70 dark:bg-green-50/10 backdrop-blur-sm p-2 mb-4 rounded-sm">
         <h2 className="font-bold text-green-500">{works.section}</h2>
       </div>
@@ -52,14 +48,23 @@ const projects = works.items
 
             <hr className="my-2 dark:text-green-500/40 text-gray-300" />
             <div className="flex gap-2 justify-start">
-              <a href={project.link_web} target="_blank" className="btn btn-circle">
+              <a
+                href={project.link_web}
+                target="_blank"
+                className="btn btn-circle"
+              >
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-arrow-down-left rotate-180" viewBox="0 0 16 16" > <path fillRule="evenodd" d="M2 13.5a.5.5 0 0 0 .5.5h6a.5.5 0 0 0 0-1H3.707L13.854 2.854a.5.5 0 0 0-.708-.708L3 12.293V7.5a.5.5 0 0 0-1 0z" /> </svg>
               </a>
-              <a href={project.link_github} target="_blank" className="btn btn-circle">
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-github" viewBox="0 0 16 16" > <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27s1.36.09 2 .27c1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.01 8.01 0 0 0 16 8c0-4.42-3.58-8-8-8" /> </svg>
+              <a
+                href={project.link_github}
+                target="_blank"
+                className="btn btn-circle"
+              >
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-github" viewBox="0 0 16 16" > <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27s1.36.09 2 .27c1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.01 8.01 0 0 0 16 8c0-4.42-3.58-8-8-8" />{" "} </svg>
               </a>
             </div>
-          </motion.div></AnimatePresence>
+          </motion.div>
+        </AnimatePresence>
         <AnimatePresence mode="wait">
           <motion.div
             key={project.id + "-image"}
