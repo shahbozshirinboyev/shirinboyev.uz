db789be4294839d85a9e9269f00f68519bd11661
shahbozshirinboyev <shahboz.sh.b@gmail.com>
2025-07-11 00:07:21 +0500

    fix some design

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -11,26 +11,33 @@ function Services() {
         <h2>{services.section}</h2>
       </div>
 
-      <div className="grid grid-cols-1 laptop:grid-cols-2 gap-14">
+      <div className="grid grid-cols-1 laptop:grid-cols-2 gap-14 items-stretch">
         {services.items.map((item, index) => (
-          <div key={index} className="group">
-            <a href={item.link} target="_blank">
-              <div className="flex justify-between items-center">
-                <span className="text-5xl font-extrabold number__font-services group-hover:text-green-500">{item.number}</span>
-                <div className="rotate-45 group-hover:rotate-0 transition-all duration-300">
-                  <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" className="bi bi-arrow-right-circle-fill group-hover:text-green-500" viewBox="0 0 16 16">
-                    <path d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0M4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5z" />
-                  </svg>
+          <div key={index} className="group h-full">
+            <a href={item.link} target="_blank" className="flex flex-col justify-between h-full">
+              <div>
+                <div className="flex justify-between items-center">
+                  <span className="text-5xl font-extrabold number__font-services group-hover:text-green-500">
+                    {item.number}
+                  </span>
+                  <div className="rotate-45 group-hover:rotate-0 transition-all duration-300">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" className="bi bi-arrow-right-circle-fill group-hover:text-green-500" viewBox="0 0 16 16">
+                      <path d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0M4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5z" />
+                    </svg>
+                  </div>
                 </div>
+                <h2 className="text-2xl laptop:text-3xl font-extrabold mt-3 group-hover:text-green-500">{item.title}</h2>
               </div>
-              <h2 className="text-2xl laptop:text-3xl font-extrabold mt-3 group-hover:text-green-500">{item.title}</h2>
-              <p className="mt-12 text-md laptop:text-lg opacity-50">{item.description}</p>
-              <hr className="opacity-50 mt-4" />
+              <div>
+                <p className="mt-12 text-md laptop:text-lg opacity-50">{item.description}</p>
+                <hr className="opacity-50 mt-3" />
+              </div>
             </a>
           </div>
         ))}
       </div>
-      
+
+
     </div>
   )
 }
