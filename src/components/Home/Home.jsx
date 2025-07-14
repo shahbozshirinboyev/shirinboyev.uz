40583d647a2733ab3372dba01d5d5d4df525bc69
shahbozshirinboyev <shahboz.sh.b@gmail.com>
2025-07-14 11:01:24 +0500

    change cv url

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -20,7 +20,7 @@ function Home() {
               <p className="font-normal mt-4">{home.description}</p> </div>
 
             <div className="flex gap-2 mt-5 justify-center laptop:justify-start">
-              <a href="./cv/shirinboyev_shahboz_cv_ru.pdf" download className="btn rounded-full flex gap-2 border border-green-200 bg-green-50 dark:bg-green-50/10 dark:border-green-200/10 text-green-500 hover:bg-green-500 hover:text-white hover:border-transparent">
+              <a href="/cv/shirinboyev_shahboz_cv_ru.pdf" download className="btn rounded-full flex gap-2 border border-green-200 bg-green-50 dark:bg-green-50/10 dark:border-green-200/10 text-green-500 hover:bg-green-500 hover:text-white hover:border-transparent">
                 <span>{home.downloadCV}</span>
                 <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-download" viewBox="0 0 16 16"> <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5" /> <path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708z" /> </svg>
               </a>
